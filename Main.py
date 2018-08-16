51d51726cf70176b235a7f5731f5afcbe90e0624
Daniel Snitkovskiy <snitkdan@uw.edu>
2018-08-16 13:56:00 -0700

    finish EmpircalDistro, add Unigram distro

--- Main.py
+++ Main.py
@@ -1,12 +1,29 @@
 from EmpiricalDistribution import EmpiricalDistribution
+from Unigram import Unigram
+import pdb
 
+brown_clean = './corpora/clean/brown_clean.txt'
+
+def head(n, prob_table):
+    c = 0
+    for i in prob_table:
+        print(i, prob_table[i])
+        c += 1
+        if c == n:
+            break
 
 def run_empirical():
-    e = EmpiricalDistribution('./corpora/clean/brown_clean.txt')
+    e = EmpiricalDistribution(brown_clean)
     e.train()
-    for k in e.prob_table:
-        print(k, e.prob_table[k])
-        break
+    head(5, e.prob_table)
+
+def run_unigram():
+    u = Unigram(brown_clean)
+    u.train()
+    head(10, u.prob_table)
+
 
 if __name__ == '__main__':
     run_empirical()
+    print('\n')
+    run_unigram()
--- Unigram.py
+++ Unigram.py
@@ -1,24 +1,29 @@
 from LanguageModel import LanguageModel
 from collections import Counter
+import numpy as np
 
 
-class EmpiricalDistribution(LanguageModel):
+class Unigram(LanguageModel):
 
     def __init__(self, corpus_path=None):
         self.prob_table = None
-        super(EmpiricalDistribution, self).__init__(corpus_path, 'EmpiricalModel')
+        super(Unigram, self).__init__(corpus_path, 'UnigramModel')
 
     def predict(self, tokens):
         if self.prob_table:
-            return self.prob_table[tokens]
+            prob = 0.0
+            for token in tokens:
+                prob += np.log(self.prob_table[token])
+            return prob
         return 0.0
 
     def train(self):
         if self.is_valid_corpus():
             self.prob_table = Counter()
             for s in open(self.corpus_path, 'r'):
-                token = [s.split('\n')[0]]
+                token = s.strip('\n').split(' ')
                 self.prob_table.update(token)
+            self.prob_table['stop'] = 1
             total = 1.0 * sum(self.prob_table.values())
             for k in self.prob_table:
                 self.prob_table[k] /= total
